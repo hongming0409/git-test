9b8340b998c636653af11bddda995a48ac2bf29c
hongming <1425802536@qq.com>
2021-05-14 17:16:09 +0800

    first commit

---
+++ src/main/java/com/hongming/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hongming.git;
+
+/**
+ * @author 王章强
+ * @create 2021-05-14 17:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
